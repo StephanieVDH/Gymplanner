5a575a345ee5c6b0dd9e946ab824b38a4cc6bec9
StephanieVDH <r0762869@student.thomasmore.be>
2025-05-12 11:40:44 +0200

    Register page

---
+++ RegisterWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gymplanner
+{
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            string email = EmailTextBox.Text;
+            string password = PasswordBox.Password;
+            DateTime? dob = DobPicker.SelectedDate;
+
+            // TODO: Validate inputs
+            // TODO: Insert new user into database
+            // If successful, perhaps open MainWindow:
+            // var main = new MainWindow();
+            // main.Show();
+            // this.Close();
+        }
+    }
+}
