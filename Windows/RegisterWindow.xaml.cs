6107e9dfd438a6a33c70799a572f6f2e642f233b
Raisa27 <145748453+Raisa27@users.noreply.github.com>
2025-05-24 13:11:00 +0200

    eye icon

--- Windows/RegisterWindow.xaml.cs
+++ Windows/RegisterWindow.xaml.cs
@@ -19,9 +19,105 @@ namespace Gymplanner.Windows
 {
     public partial class RegisterWindow : Window
     {
+        private bool isPasswordVisible = false;
+        private bool isConfirmPasswordVisible = false;
+        private bool isUpdatingPassword = false;
+        private bool isUpdatingConfirmPassword = false;
+
+
         public RegisterWindow()
         {
             InitializeComponent();
+        }
+
+        // Password visibility toggle
+        private void PasswordEyeIcon_Click(object sender, MouseButtonEventArgs e)
+        {
+            isPasswordVisible = !isPasswordVisible;
+
+            if (isPasswordVisible)
+            {
+                // Show password as text
+                PasswordTextBox.Text = PasswordBox.Password;
+                PasswordBox.Visibility = Visibility.Collapsed;
+                PasswordTextBox.Visibility = Visibility.Visible;
+                PasswordTextBox.Focus();
+                PasswordTextBox.CaretIndex = PasswordTextBox.Text.Length;
+            }
+            else
+            {
+                // Hide password
+                PasswordBox.Password = PasswordTextBox.Text;
+                PasswordTextBox.Visibility = Visibility.Collapsed;
+                PasswordBox.Visibility = Visibility.Visible;
+                PasswordBox.Focus();
+            }
+        }
+
+        // Confirm Password visibility toggle
+        private void ConfirmPasswordEyeIcon_Click(object sender, MouseButtonEventArgs e)
+        {
+            isConfirmPasswordVisible = !isConfirmPasswordVisible;
+
+            if (isConfirmPasswordVisible)
+            {
+                // Show password as text
+                ConfirmPasswordTextBox.Text = ConfirmPasswordBox.Password;
+                ConfirmPasswordBox.Visibility = Visibility.Collapsed;
+                ConfirmPasswordTextBox.Visibility = Visibility.Visible;
+                ConfirmPasswordTextBox.Focus();
+                ConfirmPasswordTextBox.CaretIndex = ConfirmPasswordTextBox.Text.Length;
+            }
+            else
+            {
+                // Hide password
+                ConfirmPasswordBox.Password = ConfirmPasswordTextBox.Text;
+                ConfirmPasswordTextBox.Visibility = Visibility.Collapsed;
+                ConfirmPasswordBox.Visibility = Visibility.Visible;
+                ConfirmPasswordBox.Focus();
+            }
+        }
+
+        // Sync password changes between PasswordBox and TextBox
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!isUpdatingPassword)
+            {
+                isUpdatingPassword = true;
+                PasswordTextBox.Text = PasswordBox.Password;
+                isUpdatingPassword = false;
+            }
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!isUpdatingPassword)
+            {
+                isUpdatingPassword = true;
+                PasswordBox.Password = PasswordTextBox.Text;
+                isUpdatingPassword = false;
+            }
+        }
+
+        // Sync confirm password changes between PasswordBox and TextBox
+        private void ConfirmPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!isUpdatingConfirmPassword)
+            {
+                isUpdatingConfirmPassword = true;
+                ConfirmPasswordTextBox.Text = ConfirmPasswordBox.Password;
+                isUpdatingConfirmPassword = false;
+            }
+        }
+
+        private void ConfirmPasswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!isUpdatingConfirmPassword)
+            {
+                isUpdatingConfirmPassword = true;
+                ConfirmPasswordBox.Password = ConfirmPasswordTextBox.Text;
+                isUpdatingConfirmPassword = false;
+            }
         }
 
         private void SignUpButton_Click(object sender, RoutedEventArgs e)
