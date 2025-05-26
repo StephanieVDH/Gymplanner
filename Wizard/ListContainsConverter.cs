e37e7fc60ed2b6130c45c40f03bc7abd51a518b4
StephanieVDH <r0762869@student.thomasmore.be>
2025-05-26 12:04:38 +0200

    functional muscle picker yay

---
+++ Wizard/ListContainsConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Gymplanner.Wizard
+{
+    /// <summary>
+    /// values[0] = IList SelectedMuscles
+    /// values[1] = string this muscle name
+    /// Two‐way: when unchecked, remove; when checked, add.
+    /// </summary>
+    public class ListContainsConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 &&
+                values[0] is IList list &&
+                values[1] is string muscle)
+            {
+                return list.Contains(muscle);
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            if (targetTypes.Length == 2 &&
+                value is bool isChecked &&
+                parameter is not null)
+            {
+                // We ignore parameter here—list & muscle come from values[]
+                // But WPF only gives us `value` and parameter in ConvertBack,
+                // so we’ll return the same two values, mutating the list as side-effect.
+            }
+            // We won’t implement ConvertBack here because WPF MultiBinding
+            // convert-back is awkward—so we’ll instead handle check/uncheck
+            // via a command on the CheckBox.Click in the XAML below.
+            throw new NotSupportedException();
+        }
+    }
+}
+
--- Wizard/WizardViewModel.cs
+++ Wizard/WizardViewModel.cs
@@ -9,21 +9,21 @@ namespace Gymplanner.Wizard
 {
     public enum Questions
     {
-        Level,        // fitnessniveau
-        Goal,         // afvallen, conditie, …
-        Sessions,     // aantal sessies per week
-        Duration,     // duur van de sessies
-        Focus        // lichaamsdeel of spiergroepen kiezen
+        Level,
+        Goal,
+        Sessions,
+        Duration,
+        Focus,       // we merged Focus + FocusMuscles into one step
     }
 
     public class WizardViewModel : INotifyPropertyChanged
     {
-        // The list of steps in order
+        // 1) The list of steps
         public ObservableCollection<Questions> Steps { get; }
             = new ObservableCollection<Questions>(
                 Enum.GetValues(typeof(Questions)).Cast<Questions>());
 
-        // Current step index & derived enum
+        // 2) Current index / step
         private int _currentIndex;
         public int CurrentIndex
         {
@@ -38,58 +38,87 @@ namespace Gymplanner.Wizard
         }
         public Questions CurrentStep => Steps[CurrentIndex];
 
-        // Navigation commands
+        // 3) Navigation commands
         public ICommand NextCommand { get; }
         public ICommand PrevCommand { get; }
 
-        // Selection commands for each question
+        // 4) Selection commands for the first half of the wizard
         public ICommand SelectLevelCommand { get; }
         public ICommand SelectGoalCommand { get; }
         public ICommand SelectSessionsCommand { get; }
         public ICommand SelectDurationCommand { get; }
         public ICommand SelectFocusCommand { get; }
-        public ICommand SelectMusclesCommand { get; }
+        public ICommand CommitMuscleSelectionCommand { get; }
+        public ICommand ToggleMuscleCommand { get; }
 
-        // Properties to hold the user’s answers
+        // 6) Data holders for the user’s answers
         public string SelectedLevel { get; private set; }
         public string SelectedGoal { get; private set; }
         public string SelectedSessions { get; private set; }
         public string SelectedDuration { get; private set; }
         public string SelectedFocus { get; private set; }
-        public string SelectedMuscles { get; private set; }
+
+        // 7) The full list of muscle‐group options
+        public ObservableCollection<string> AllMuscleOptions { get; }
+            = new ObservableCollection<string>
+            {
+                "Back",
+                "Chest",
+                "Shoulders",
+                "Core/Abs",
+                "Biceps",
+                "Forearms",
+                "Triceps",
+                "Calves",
+                "Glutes",
+                "Hamstrings",
+                "Quadriceps",
+                "Hip Abductors",
+                "Hip Adductors"
+            };
+
+        // 8) Which ones the user has checked
+        public ObservableCollection<string> SelectedMuscles { get; }
+            = new ObservableCollection<string>();
 
         public WizardViewModel()
         {
-            // Initialize selection commands to capture answer & advance
+            // wire up the basic selection commands (auto‐advance)
             SelectLevelCommand = new RelayCommand(p => { SelectedLevel = p?.ToString(); AdvanceStep(); });
             SelectGoalCommand = new RelayCommand(p => { SelectedGoal = p?.ToString(); AdvanceStep(); });
             SelectSessionsCommand = new RelayCommand(p => { SelectedSessions = p?.ToString(); AdvanceStep(); });
             SelectDurationCommand = new RelayCommand(p => { SelectedDuration = p?.ToString(); AdvanceStep(); });
-            SelectFocusCommand = new RelayCommand(p => { SelectedFocus = p?.ToString(); AdvanceStep(); });
-            SelectMusclesCommand = new RelayCommand(p => { SelectedMuscles = p?.ToString(); AdvanceStep(); });
+            SelectFocusCommand = new RelayCommand(p => { SelectedFocus = p?.ToString(); /* do NOT advance here if you want them to pick muscles too */ });
+            ToggleMuscleCommand = new RelayCommand(p =>
+            {
+                var muscle = p?.ToString();
+                if (muscle == null) return;
+                if (SelectedMuscles.Contains(muscle))
+                    SelectedMuscles.Remove(muscle);
+                else
+                    SelectedMuscles.Add(muscle);
+            });
 
-            // Navigation commands
-            NextCommand = new RelayCommand(_ => AdvanceStep(),
-                                           _ => CurrentIndex < Steps.Count - 1);
-            PrevCommand = new RelayCommand(_ => RetreatStep(),
-                                           _ => CurrentIndex > 0);
+            // CommitMuscleSelection only advances once they hit “Done”
+            CommitMuscleSelectionCommand = new RelayCommand(_ => AdvanceStep());
+
+            // standard next/prev (if you still need them)
+            NextCommand = new RelayCommand(_ => AdvanceStep(), _ => CurrentIndex < Steps.Count - 1);
+            PrevCommand = new RelayCommand(_ => RetreatStep(), _ => CurrentIndex > 0);
         }
 
-        // Move forward one step
         private void AdvanceStep()
         {
             if (CurrentIndex < Steps.Count - 1)
                 CurrentIndex++;
         }
 
-        // Move backward one step
         private void RetreatStep()
         {
             if (CurrentIndex > 0)
                 CurrentIndex--;
         }
 
-        // INotifyPropertyChanged boilerplate
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string prop = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
@@ -100,20 +129,22 @@ namespace Gymplanner.Wizard
     {
         private readonly Action<object> _execute;
         private readonly Predicate<object> _canExecute;
-        public RelayCommand(Action<object> execute, Predicate<object>? canExecute = null)
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute ?? (_ => true);
         }
 
-        public bool CanExecute(object? parameter) => _canExecute(parameter!);
-        public void Execute(object? parameter) => _execute(parameter!);
-        public event EventHandler? CanExecuteChanged
+        public bool CanExecute(object parameter) => _canExecute(parameter);
+        public void Execute(object parameter) => _execute(parameter);
+        public event EventHandler CanExecuteChanged
         {
-            add => CommandManager.RequerySuggested += value!;
-            remove => CommandManager.RequerySuggested -= value!;
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
         }
     }
 }
+
 
 
